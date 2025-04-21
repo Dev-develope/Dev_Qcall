0d6f68d9783344e980436ef5762381244adc1d1d
Dev-develope <devendra.vishwkarma@engineermaster.in>
2025-04-21 11:01:14 +0530

    Bread_Crub-sitemapxml are added

--- src/components/common/HeroBackgroundImage.jsx
+++ src/components/common/HeroBackgroundImage.jsx
@@ -2,8 +2,9 @@
 import Image from "next/image";
 import Navbar from "./Navbar";
 import privcayImg from "../../../public/images/blog.png";
+import Breadcrumb from "./Breadcrumb";
 
-export default function HeroBackgroundImage({ data,img=privcayImg }) {
+export default function HeroBackgroundImage({ data, img = privcayImg ,children}) {
   return (
     <div className="relative w-full h-[25rem] overflow-hidden">
       <Navbar />
@@ -15,8 +16,6 @@ export default function HeroBackgroundImage({ data,img=privcayImg }) {
         priority
       />
 
-
-   
       <div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col justify-center items-center text-white text-center px-4">
         <p className="px-4 py-2 rounded-full text-xs mb-4 border border-gray-400 backdrop-blur-sm bg-white/10">
           {data.para}
@@ -27,8 +26,12 @@ export default function HeroBackgroundImage({ data,img=privcayImg }) {
         <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold drop-shadow-md">
           {data.heading2}
         </h1>
+
+        <div>
+          {/* <Breadcrumb /> */}
+          {children}
+        </div>
       </div>
     </div>
   );
 }
-
