1fb31271c48f8ca6ad0d3d1cd8445fd38f8cfb63
Dev-develope <devendra.vishwkarma@engineermaster.in>
2025-04-23 17:09:27 +0530

    animation_done_in future_card_real_state

--- src/components/common/FarmerMotion.jsx
+++ src/components/common/FarmerMotion.jsx
@@ -45,7 +45,22 @@ const FarmerMotionCardCenter = ({ children }) => {
   );
 };
 
+
+const FarmerMotionCardCenter1 = ({ children }) => {
+  return (
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    viewport={{ once: true }}
+    transition={{ duration: 0.6 }}
+  >
+   {children}
+  </motion.div>
+  
+  );
+};
+
 export default FarmerMotionCard;
 
 
-export  {FarmerMotion,FarmerMotionCard,FarmerMotionCardCenter};
+export  {FarmerMotion,FarmerMotionCard,FarmerMotionCardCenter,FarmerMotionCardCenter1};
